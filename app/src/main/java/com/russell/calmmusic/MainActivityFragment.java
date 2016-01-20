96fa46db9ba296c709f7e82e236aa36e6d4d2fc7
Bertrand <1308786830@qq.com>
2016-01-21 00:32:37 +0800

    fragment random player
    fragment random player

--- app/src/main/java/com/russell/calmmusic/MainActivityFragment.java
+++ app/src/main/java/com/russell/calmmusic/MainActivityFragment.java
@@ -1,28 +1,38 @@
 package com.russell.calmmusic;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.content.ContentResolver;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.russell.calmmusic.bean.MusicInfo;
+import com.russell.calmmusic.tools.MusicLoader;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements MediaPlayer.OnCompletionListener {
 
     private HomeAdapter mAdapter;
-    private List<String> mDatas;
+    private List<String> mDatas = new ArrayList<>();
     private RecyclerView recycler;
+    private ContentResolver contentResolver;
+    private MusicLoader musicLoader;
+    private List<?> list;
+    private Random random = new Random();
+    private int position = 0;
 
     public MainActivityFragment() {
-
     }
 
     @Override
@@ -34,16 +44,31 @@ public class MainActivityFragment extends Fragment {
         return view;
     }
 
-    private void initView(View view){
+    private void initView(View view) {
         recycler = (RecyclerView) view.findViewById(R.id.recycler_view);
-        recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recycler.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
         recycler.setAdapter(mAdapter = new HomeAdapter());
     }
+
     protected void initData() {
-        mDatas = new ArrayList<String>();
-        for (int i = 'A'; i < 'z'; i++) {
-            mDatas.add("" + (char) i);
+        contentResolver = getActivity().getContentResolver();
+        musicLoader = new MusicLoader(contentResolver);
+        list = musicLoader.getMusicList();
+        for (int i = 0; i < list.size(); i++) {
+            MusicInfo musicInfo = (MusicInfo) list.get(i);
+            String a = musicInfo.getUrl();
+//            Log.i("url", a);
         }
+        position = random.nextInt(Math.abs(list.size()));
+        musicLoader.musicPlayer(((MusicInfo) list.get(position)).getUrl(), list);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        position = random.nextInt(Math.abs(list.size()));
+        musicLoader.musicPlayer(((MusicInfo) list.get(position)).getUrl(), list);
     }
 
     class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
@@ -66,6 +91,7 @@ public class MainActivityFragment extends Fragment {
 
         class MyViewHolder extends RecyclerView.ViewHolder {
             TextView tv;
+
             public MyViewHolder(View itemView) {
                 super(itemView);
                 tv = (TextView) itemView.findViewById(R.id.recycler_list_item);
---
+++ app/src/main/java/com/russell/calmmusic/tools/MusicLoader.java
@@ -0,0 +1,105 @@
+package com.russell.calmmusic.tools;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore.Audio.Media;
+import android.util.Log;
+
+import com.russell.calmmusic.bean.MusicInfo;
+
+public class MusicLoader implements MediaPlayer.OnCompletionListener{
+
+    private static final String TAG = "com.example.nature.MusicLoader";
+    private static boolean completion = false;
+    private static List<MusicInfo> musicList = new ArrayList<MusicInfo>();
+    //Uri，指向external的database  
+    private Uri contentUri = Media.EXTERNAL_CONTENT_URI;
+    //projection：选择的列; where：过滤条件; sortOrder：排序。
+    private String[] projection = {
+            Media._ID,
+            Media.DISPLAY_NAME,
+            Media.DATA,
+            Media.ALBUM,
+            Media.ARTIST,
+            Media.DURATION,
+            Media.SIZE
+    };
+    private String where = "mime_type in ('audio/mpeg','audio/x-ms-wma') and is_music > 0 ";
+    private String sortOrder = Media.DATA;
+
+    public MusicLoader(ContentResolver contentResolver) {                                                                                                             //利用ContentResolver的query函数来查询数据，然后将得到的结果放到MusicInfo对象中，最后放到数组中
+        Cursor cursor = contentResolver.query(contentUri, projection, where, null, sortOrder);
+        System.out.println(cursor);
+        if (cursor == null) {
+            Log.v(TAG, "Line(37  )   Music Loader cursor == null.");
+        } else if (!cursor.moveToFirst()) {
+            Log.v(TAG, "Line(39  )   Music Loader cursor.moveToFirst() returns false.");
+        } else {
+            int displayNameCol = cursor.getColumnIndex(Media.DISPLAY_NAME);
+            int albumCol = cursor.getColumnIndex(Media.ALBUM);
+            int idCol = cursor.getColumnIndex(Media._ID);
+            int durationCol = cursor.getColumnIndex(Media.DURATION);
+            int sizeCol = cursor.getColumnIndex(Media.SIZE);
+            int artistCol = cursor.getColumnIndex(Media.ARTIST);
+            int urlCol = cursor.getColumnIndex(Media.DATA);
+            do {
+                String title = cursor.getString(displayNameCol);
+                String album = cursor.getString(albumCol);
+                long id = cursor.getLong(idCol);
+                int duration = cursor.getInt(durationCol);
+                long size = cursor.getLong(sizeCol);
+                String artist = cursor.getString(artistCol);
+                String url = cursor.getString(urlCol);
+
+                MusicInfo musicInfo = new MusicInfo(id, title);
+                musicInfo.setAlbum(album);
+                musicInfo.setDuration(duration);
+                musicInfo.setSize(size);
+                musicInfo.setArtist(artist);
+                musicInfo.setUrl(url);
+                musicList.add(musicInfo);
+
+            } while (cursor.moveToNext());
+        }
+    }
+
+    public List<MusicInfo> getMusicList() {
+        return musicList;
+    }
+
+    public Uri getMusicUriById(long id) {
+        Uri uri = ContentUris.withAppendedId(contentUri, id);
+        return uri;
+    }
+    public void musicPlayer(String url, List<?> list){
+        /**
+         * 播放音乐
+         */
+        Log.i("url=======", url);
+        final MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setLooping(false); /*循环*/
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setLooping(completion); /*循环*/
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepare(); // might take long! (for buffering, etc)
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+}
